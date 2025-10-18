36c3db9105feadc89f1b7c0f18fee25041a2e01f
zhouxiaohe168 <tjkj10188@gmail.com>
2025-10-18 23:00:44 +0800

    Add Home component for TV Trade Bot

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+export default function Home() {
+  return <div style={{fontFamily:'sans-serif',padding:24}}>
+    <h1>TV Trade Bot</h1>
+    <p>接口就绪：<a href="/api/webhook">/api/webhook</a></p>
+  </div>;
+}
